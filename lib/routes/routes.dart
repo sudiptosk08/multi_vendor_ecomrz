0371a95db4a5ff6572fc84981457230d892a7c13
sudiptosk08 <sudiptosk08@gmail.com>
2023-03-23 17:52:33 +0600

    update

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -4,9 +4,12 @@ import 'package:ecommerce_app/app/view/screens/auth/login_page.dart';
 import 'package:ecommerce_app/app/view/screens/home/home_page.dart';
 import 'package:get/get.dart';
 
+import '../app/view/screens/cart/widget/cart_page.dart';
+
 class Routes {
   static const loginPage = '/loginPage';
   static const homePage = '/homePage';
+  static const cartPage = '/cartPage';
 
   static var list = [
     GetPage(
@@ -16,6 +19,10 @@ class Routes {
     GetPage(
       name:homePage,
       page: () =>const HomePage(),
+    ),
+     GetPage(
+      name: cartPage,
+      page: () => const CartPage(),
     ),
   ];
 }
